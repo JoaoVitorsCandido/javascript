14013c3877356abc4f743b90ecc7f8c97d85f069
joaovitorcandido <joaovitorsocandido@gmail.com>
2025-11-04 00:08:28 -0300

    exercicios

--- cursoudemy/index e strings/index01/index.js
+++ cursoudemy/index e strings/index01/index.js
@@ -1,4 +1,14 @@
 let umaString = "umma string"
+const pag = document.body
 
 console.log(umaString.indexOf("m"))
 console.log(umaString.lastIndexOf("m"))
+
+const num1 = prompt("digite um numero")
+const num2 = prompt("digite outro numero")
+
+let num1res = Number(num1)
+let num2res = Number(num2)
+let soma = num1res + num2res
+
+alert(` a soma do ${num1res} + ${num2res} é igual a ${soma}`)
