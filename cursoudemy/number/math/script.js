99053a7aac781c22e544b15edd0cb9def139cdba
joaovitorcandido <joaovitorsocandido@gmail.com>
2025-11-06 22:12:12 -0300

    aprendendo sobre o método Math

---
+++ cursoudemy/number/math/script.js
@@ -0,0 +1,8 @@
+const num1 = 100.5472
+
+console.log(Math.ceil(num1))
+console.log(Math.floor(num1))
+console.log(Math.round(num1))
+console.log(Math.max(1, 2, 6, 8))
+console.log(Math.min(1, 3, 8, -5))
+console.log(Math.random() * (100 - 5) +5)
